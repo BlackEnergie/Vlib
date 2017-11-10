56ce921d8e7acbe778a401102afa5c9f1dd3dfe4
RyannGira <giraryann@outlook.fr>
2017-11-10 16:18:46 +0100

    Ajout Vue de la maintenance

---
+++ vues/maintenance.php
@@ -0,0 +1,35 @@
+<div id="conteneur">
+
+    <div id="header">
+        <?php include 'haut.php' ;?>
+    </div>
+
+    <div id="content">
+        <div id="classement">
+            <title>Liste des Stations</title>
+            <table class="tab">
+                <thead>
+                <tr>
+                    <td>N°</td>
+                    <td>Nom</td>
+                    <td>Vélo</td>
+                    <td>Places</td>
+                    <td></td>
+                    <td></td>
+                    <td>Etat</td>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                /* Création du tableau avec l'etat des station,... et d'un select pour l'etat de la station (dernière colonne) */
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div id="bas">
+        <?php  include 'bas.php' ;?>
+    </div>
+
+</div>

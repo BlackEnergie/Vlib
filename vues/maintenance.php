13f74de69f5c6a636af4f1a6fe0e383ef29a12b9
RyannGira <giraryann@outlook.fr>
2017-11-10 16:58:29 +0100

    Liste des stations dans station et maintenance

--- vues/maintenance.php
+++ vues/maintenance.php
@@ -21,7 +21,21 @@
                 </thead>
                 <tbody>
                 <?php
-                /* Création du tableau avec l'etat des station,... et d'un select pour l'etat de la station (dernière colonne) */
+                    /* Création du tableau avec le choix de l'etat des station, le n°, le nom, le nombre de velo disponible, le nombre de places, ... */
+                    $tab = "";
+                    $listeStation = StationDAO::lesStations();
+                    if(count($listeStation) > 0){
+                        foreach($listeStation as $station){
+                            $tab .= <tr><td>$station[0]</td>
+                                        <td>$station[2]</td>
+                                        <td>$station[4]</td>
+                                        <td>$station[5]</td>
+                                        <td></td>
+                                        <td>$station[3]</td>
+                                        <td>creer select etat</td></tr>;
+                            }
+                    }
+                    return $tab;
                 ?>
                 </tbody>
             </table>

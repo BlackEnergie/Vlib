27b4d76ac92cd6fc2b99d48819e9659207446c0d
BlackEnergie <the.perrin@outlook.fr>
2017-11-18 12:10:01 +0100

    modif mineures

--- controleurs/ControleurInscription.php
+++ controleurs/ControleurInscription.php
@@ -1,24 +1,23 @@
 <?php
 
-
 //création du formulaire d'inscription (post indique que les données seront recupérées)
 
-$formulaireConnexion = new Formulaire('post', 'index.php', 'fInscript', 'fInscript');
+$formulaireInscription = new Formulaire('post', 'index.php', 'fInscript', 'fInscript');
 
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabelFor('identifiant', 'Identifiant :'), 1);
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerInputTexte('login', 'login', '', 1, '', 0), 1);
-$formulaireConnexion->ajouterComposantTab();
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerLabelFor('identifiant', 'Identifiant :'), 1);
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerInputTexte('login', 'login', '', 1, '', 0), 1);
+$formulaireInscription->ajouterComposantTab();
 
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabelFor('mdp', 'Mot de passe :'), 1);
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerInputMdp('mdp', 'mdp', '', 1, '', 0), 1);
-$formulaireConnexion->ajouterComposantTab();
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerLabelFor('mdp', 'Mot de passe :'), 1);
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerInputMdp('mdp', 'mdp', '', 1, ''), 1);
+$formulaireInscription->ajouterComposantTab();
 
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerInputSubmit('submitConnex', 'submitConnex', 'Valider'), 2);
-$formulaireConnexion->ajouterComposantTab();
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerInputSubmit('submitConnex', 'submitConnex', 'Valider'), 2);
+$formulaireInscription->ajouterComposantTab();
 
-$formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabel($messageErreurConnexion, "messageErreurConnexion"), 2);
-$formulaireConnexion->ajouterComposantTab();
+$formulaireInscription->ajouterComposantLigne($formulaireInscription->creerLabel($messageErreurConnexion, "messageErreurConnexion"), 2);
+$formulaireInscription->ajouterComposantTab();
 
-$formulaireConnexion->creerFormulaire();
+$formulaireInscription->creerFormulaire();
 
  include_once 'vues/Accueil.php';

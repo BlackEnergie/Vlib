132cb470ff26f597affe559e22cbdabc3ccf493f
imad <Im.benbadda@gmail.com>
2017-11-22 13:15:59 +0100

    correction

--- configs/param.php
+++ configs/param.php
@@ -2,7 +2,7 @@
 //D�finition des variables de connexion
 class Param {
 	public static $user = 'root';
-	public static $pass = 'root';
+  public static $pass = '1234';
 	public static $dsn = 'mysql:host=127.0.0.1:8889;dbname=perrint_vlib;charset=utf8';
 }
 ?>

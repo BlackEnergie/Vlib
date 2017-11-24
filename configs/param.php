b5c79e2d6415956e24a8dc720301484437c7dbcb
RyannGira <giraryann@outlook.fr>
2017-11-24 14:44:22 +0100

    param ryann

--- configs/param.php
+++ configs/param.php
@@ -1,8 +1,8 @@
 <?php
 //D�finition des variables de connexion
 class Param {
-	public static $user = 'root';
-	public static $pass = '';
-	public static $dsn = 'mysql:host=127.0.0.1;dbname=vlib;charset=utf8';
+    public static $user = 'root';
+    public static $pass = '';
+    public static $dsn = 'mysql:host=127.0.0.1;dbname=vlib;charset=utf8';
 }
 ?>

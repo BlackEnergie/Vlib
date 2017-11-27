5e32eb2ecbf91d89edfcab7ccb7a44b044d872c0
imad <Im.benbadda@gmail.com>
2017-11-27 18:55:10 +0100

    a

--- configs/param.php
+++ configs/param.php
@@ -5,5 +5,5 @@ class Param {
 	// mettre $pass=''
   public static $pass = 'root';
 	// supprimer :8889 de $dsn car numero du port pour mac
-	public static $dsn = 'mysql:host=127.0.0.1;dbname=perrint_vlib;charset=utf8';
+	public static $dsn = 'mysql:host=127.0.0.1:8889;dbname=perrint_vlib;charset=utf8';
 }
